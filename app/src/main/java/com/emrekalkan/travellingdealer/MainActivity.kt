0b4f2a5f5918b18a0c26cbdc3ac006de7ca2ef4f
emre.kalkan <emre.kalkan@univera.com.tr>
2020-12-02 01:14:53 +0300

    Shortest path

--- app/src/main/java/com/emrekalkan/travellingdealer/MainActivity.kt
+++ app/src/main/java/com/emrekalkan/travellingdealer/MainActivity.kt
@@ -4,64 +4,109 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.button.MaterialButton
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
-    private val customers = arrayListOf<Node>()
+    private lateinit var deleteButton: MaterialButton
+    private lateinit var calculateButton: MaterialButton
+
+    private val customers = LinkedList<Node>()
     private val factoryCoordinate = Coordinate(3, 7)
-    private val startNode = Node(Coordinate(0, 0))
+    private var finalNode = Node(Coordinate(0, 0))
+    private var distance = Double.MAX_VALUE
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setCustomerCoordinates()
 
-        val deleteButton = findViewById<MaterialButton>(R.id.btnDeleteNode)
+        deleteButton = findViewById(R.id.btnDeleteNode)
         deleteButton.setOnClickListener {
+            deleteButton.isEnabled = false
             val middlePos = customers.size / 2
             val middleNode = if (customers.size > middlePos) customers[middlePos] else null
             middleNode?.also {
-                startNode.deleteNode(startNode, it.coordinate)
+                finalNode.deleteNode(finalNode, it.coordinate)
                 customers.removeAt(middlePos)
-                calculateDistance()
+                calculatePermutations(customers.size, customers.toTypedArray())
+                showResult()
             }
         }
 
-        findViewById<MaterialButton>(R.id.btnCalculate).setOnClickListener {
-            calculateDistance()
+        calculateButton = findViewById(R.id.btnCalculate)
+        calculateButton.setOnClickListener {
             deleteButton.isEnabled = true
+            calculateButton.isEnabled = false
+            calculatePermutations(customers.size, customers.toTypedArray())
+            showResult()
         }
     }
 
     private fun setCustomerCoordinates() {
         customers.apply {
-            add(Node(Coordinate(2, 1)))
-            add(Node(Coordinate(5, 2)))
-            add(Node(Coordinate(9, 2)))
-            add(Node(Coordinate(1, 4)))
-            add(Node(Coordinate(8, 4)))
-            add(Node(Coordinate(6, 5)))
-            add(Node(Coordinate(8, 9)))
-            add(Node(Coordinate(1, 10)))
+            add(Node(Coordinate(1, 1)))
+            add(Node(Coordinate(2, 8)))
+            add(Node(Coordinate(10, 3)))
+            add(Node(Coordinate(6, 4)))
         }
     }
 
-    private fun calculateDistance() {
-        startNode.apply {
-            if (following == null) {
-                customers.forEach { node ->
-                    appendToEnd(node.coordinate)
-                    // Go to factory after every customer visit
-                    appendToEnd(factoryCoordinate)
-                }
-            }
-        }
-        val nodesString = "Nodes:\n${startNode.printNodes()}"
-        val totalDistance = "Distance: ${String.format("%.4f", startNode.sumOfNodes())}"
-        val length = "Length: ${startNode.length(startNode)}"
+    private fun showResult() {
+        val nodesString = "Nodes:\n${finalNode.printNodes()}"
+        val totalDistance = "Distance: ${String.format("%.4f", finalNode.sumOfNodes())}"
+        val length = "Length: ${finalNode.length(finalNode)}"
         findViewById<TextView>(R.id.tvNodes).text = nodesString
         findViewById<TextView>(R.id.tvDistance).text = totalDistance
         findViewById<TextView>(R.id.tvLength).text = length
+    }
 
+    private fun calculateDistance(elements: Array<Node>) {
+        Node(Coordinate(0, 0)).apply {
+            elements.forEach { node ->
+                appendToEnd(node.coordinate)
+                // Go to factory after every customer visit
+                appendToEnd(factoryCoordinate)
+            }
+
+            val totalDistance = sumOfNodes()
+            if (totalDistance < distance) {
+                distance = totalDistance
+                finalNode = this
+            }
+        }
+    }
+
+    private fun calculatePermutations(n: Int, elements: Array<Node>) {
+        if (n == 1) {
+            calculateDistance(elements)
+            println(elements.joinToString(
+                separator = " --> ",
+                transform = { "${it.coordinate.x}, ${it.coordinate.y}" }
+            ))
+        } else {
+            for (i in 0 until n - 1) {
+                calculatePermutations(n - 1, elements)
+                if (n % 2 == 0) {
+                    swap(elements, i, n - 1)
+                } else {
+                    swap(elements, 0, n - 1)
+                }
+            }
+            calculatePermutations(n - 1, elements)
+        }
+    }
+
+    private fun swap(input: Array<Node>, a: Int, b: Int) {
+        val tmp: Node = input[a]
+        input[a] = input[b]
+        input[b] = tmp
+    }
+
+    private fun printArray(input: Array<Node>) {
+        for (i in input.indices) {
+            println(input[i])
+        }
     }
 }
